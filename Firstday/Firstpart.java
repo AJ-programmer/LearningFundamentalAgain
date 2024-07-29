dedb950846b94459c8ad7754fef8d58565299f71
Axcel Joh H. Bernal <148522639+AJ-programmer@users.noreply.github.com>
2024-07-29 14:53:32 +0800

    Create Firstpart.java

---
+++ Firstday/Firstpart.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.firstday;
+
+public class FirstDay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.print("World");
+
+    }
+}
